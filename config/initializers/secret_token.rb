29119ebd586baf9ea9f9c3a4996c4b029681f2c5
Lauralee Flores <flores.lauralee@gmail.com>
2013-06-17 10:33:35 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nylaw::Application.config.secret_key_base = '711b108c93e454e500e1d84ed804892b1d78a0d47f715b0b9649248e3a8a86f702dff5aafb68bc3f899fa240b9fb46cb33bcb66e8da54823b9b1157b99e95966'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Nylaw::Application.config.session_store :cookie_store, key: '_nylaw_session'
